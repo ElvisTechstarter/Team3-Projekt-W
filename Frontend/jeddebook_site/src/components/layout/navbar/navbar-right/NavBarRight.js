670ea11b1f6afbcb32c769017d55600c206721e7
Micha <michael.rigas@t-online.de>
2024-04-10 16:34:32 +0200

    Header Design Fixes

--- Frontend/jeddebook_site/src/components/layout/navbar/navbar-right/NavBarRight.js
+++ Frontend/jeddebook_site/src/components/layout/navbar/navbar-right/NavBarRight.js
@@ -1,11 +1,39 @@
 import StandardBtn from "../../../common/buttons/standard-btn";
 import styles from "./NavBarRight.module.css";
+import React, { useState } from "react";
 
 function NavBarRight() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className={styles.mainContainer}>
-      <StandardBtn text={"Login"} />
+      <input
+        className={styles.username}
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={handleUsernameChange}
+      />
+      <input
+        className={styles.password}
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
 
+      <div className={styles.buttonContainer}>
+        <StandardBtn text={"Login"} />
+      </div>
       <div className={styles.spacer} />
       <StandardBtn text={"Register"} />
     </div>
