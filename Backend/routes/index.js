55811d6e45d701a31e7bf5e1f5ddae812da5168c
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-04-11 15:50:10 +0200

    DB user erstellen/löschen

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const { JeddebookRouter } = require("./jeddebook");
+const { AuthRouter } = require("./auth");
+const { UserRouter } = require("./user");
 
 const AppRouter = Router();
 
 AppRouter.use("/jeddebook", JeddebookRouter);
+AppRouter.use("/auth", AuthRouter);
+AppRouter.use("/user", UserRouter);
 
 module.exports = { AppRouter };
