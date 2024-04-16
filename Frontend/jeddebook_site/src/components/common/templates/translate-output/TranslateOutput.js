2c5f7390d5f2c119ea25ffc1d02e00690df211c9
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-04-16 13:58:15 +0200

    TranslateOutput mitbeingebracht

--- Frontend/jeddebook_site/src/components/common/templates/translate-output/TranslateOutput.js
+++ Frontend/jeddebook_site/src/components/common/templates/translate-output/TranslateOutput.js
@@ -1,7 +1,30 @@
 import styles from "./TranslateOutput.module.css";
 
 function TranslateOutput() {
-  return <div>Hier kommt der TranslateOutput hin</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div>
+        <div className={styles.titles}>
+          Die englische Übersetzung lautet:
+          <div className={styles.languageContainer}>
+            <div className={styles.Input}>Apfel</div>
+            <div className={styles.spacer}></div>
+            <div className={styles.Output}>Apple</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.titles}>
+        Mögliche Anwendungsfälle:
+        <div className={styles.Features}>
+          <div className={styles.phrases}>
+            <div>Ein Apfel am Tag hält einen gesund.</div>
+            <div className={styles.spacer}></div>
+            <div>An apple a day keeps the doctor away.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TranslateOutput;
