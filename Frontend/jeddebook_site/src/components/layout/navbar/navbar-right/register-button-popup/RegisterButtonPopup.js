e9490e7364d472b6b2d6f2c522429618f135b4ac
Micha <michael.rigas@t-online.de>
2024-04-16 14:46:41 +0200

    Design Register update

--- Frontend/jeddebook_site/src/components/layout/navbar/navbar-right/register-button-popup/RegisterButtonPopup.js
+++ Frontend/jeddebook_site/src/components/layout/navbar/navbar-right/register-button-popup/RegisterButtonPopup.js
@@ -14,10 +14,12 @@ function RegisterButtonPopup({ onClose, onRegister }) {
     <div className={styles.container} onClick={onClose}>
       <div className={styles.popup} onClick={onClickChild}>
         <h2>Register</h2>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <input type="repeat password" placeholder="Repeat password" />
-        <button onClick={handleRegister}>Register</button>
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <input type="repeat password" placeholder="repeat password" />
+        <input type="text" placeholder="e-mail address" />
+        <button onClick={handleRegister}>Let's go!</button>
+        <div style={{ height: "5px" }} />
         <button onClick={onClose}>Close</button>
       </div>
     </div>
