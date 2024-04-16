99f4f06c6e9d5fed0cf1983fd4f61ce8e6229553
cjahn1305 <152269511+cjahn1305@users.noreply.github.com>
2024-04-16 16:45:58 +0200

    UseContext

--- Frontend/jeddebook_site/src/App.js
+++ Frontend/jeddebook_site/src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavigationBar from "./components/layout/navbar";
 import "./styles/App.css";
@@ -6,20 +7,25 @@ import HomePage from "./pages/home-page";
 import ImpressumPage from "./pages/impressum-page";
 import Content from "./components/layout/content";
 import Footer from "./components/layout/footer";
+import {AuthProvider} from "./components/contexts/AuthContext"; // Import des AuthProviders
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavigationBar />
-      <Content>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/impressum" element={<ImpressumPage />} />
-        </Routes>
-      </Content>
-      <Footer />
-    </BrowserRouter>
+    // Umgebung mit dem AuthProvider um die App-Komponente herum
+    <AuthProvider>
+      <BrowserRouter>
+        <NavigationBar />
+        <Content>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/impressum" element={<ImpressumPage />} />
+          </Routes>
+        </Content>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
 export default App;
+
--- Frontend/jeddebook_site/src/components/common/buttons/checkbox/Checkbox.js
+++ Frontend/jeddebook_site/src/components/common/buttons/checkbox/Checkbox.js
@@ -1,11 +1,20 @@
+import React, { useContext } from 'react';
 import styles from "./Checkbox.module.css";
+import AuthContext from '../../../contexts/AuthContext'; // Pfade anpassen
 
-function Checkbox({ onClick, isChecked }) {
+function Checkbox({ onClick }) {
+  // Zugriff auf den Login-Status über den AuthContext
+  const { isLoggedIn } = useContext(AuthContext);
+
   return (
-    <div className={styles.mainContainer} onClick={onClick}>
-      {isChecked ? <div className={styles.isChecked} /> : null}
+    // Wenn eingeloggt, füge das onClick-Event hinzu, sonst null (kein Event)
+    <div className={styles.mainContainer} onClick={isLoggedIn ? onClick : null}>
+      {/* Wenn eingeloggt, zeige die Checkbox an, sonst null */}
+      {isLoggedIn ? <div className={styles.isChecked} /> : null}
     </div>
   );
 }
 
 export default Checkbox;
+
+
