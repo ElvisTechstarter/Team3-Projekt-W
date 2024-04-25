f4db6998e5545186ba80c0ad31f9af574d0b7ae3
Witalitechstarter <152269632+Witalitechstarter@users.noreply.github.com>
2024-04-25 13:35:21 +0200

    Update ImageSearch.js

--- Frontend/jeddebook_site/src/components/ImageSearch.js
+++ Frontend/jeddebook_site/src/components/ImageSearch.js
@@ -23,15 +23,68 @@ function ImageSearch() {
 
   return (
     <div>
-      <input type="text" placeholder='Hier soll Text rein' value={query} onChange={e => setQuery(e.target.value)} />
-      <button onClick={handleSearch}>Suche</button>
+      <style>
+        {`
+          .input-container {
+            margin-bottom: 20px;
+          }
+
+          .input-container input[type="text"] {
+            padding: 10px;
+            width: 200px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 16px;
+          }
+
+          .input-container button {
+            padding: 10px 20px;
+            background-color: #007bff;
+            border: none;
+            border-radius: 5px;
+            color: white;
+            cursor: pointer;
+            font-size: 16px;
+          }
+
+          .input-container button:hover {
+            background-color: #0056b3;
+          }
+
+          .image-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+          }
+
+          .image-container img {
+            margin-bottom: 10px;
+            max-width: 32%;
+            height: auto;
+            border-radius: 5px;
+            cursor: pointer;
+            object-fit: cover; /* Alle Bilder werden proportional skaliert */
+          }
+
+          .image-container img:hover {
+            box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
+          }
+        `}
+      </style>
+
+      <form onSubmit={e => { e.preventDefault(); }}>
+        <div className="input-container">
+          <input type="text" placeholder='Hier soll Text rein' value={query} onChange={e => setQuery(e.target.value)} />
+          <button type="submit" onClick={handleSearch}>Suche</button>
+        </div>
+      </form>
     
-      <div>
+      <div className="image-container">
         {results.map((item, index) => (
-          <img key={index} src={item.urls.small} alt={item.alt_description} onClick={() => handleImageClick(index)} />
+          <img key={index} src={item.urls.thumb} alt={item.alt_description} onClick={() => handleImageClick(index)} />
         ))}
       </div>
-    </div>
+    </div>  
   );
 }
 
