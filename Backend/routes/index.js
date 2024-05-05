c462d1150b11eb45f732c7f344b88d6882267347
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-05-05 21:18:16 +0200

    jwt authentification added

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const { JeddebookRouter } = require("./jeddebook");
 const { UserRouter } = require("./user");
+const { AuthRouter } = require("./auth");
+const authMiddleWare = require("../middlewares/authMiddleware");
 
 const AppRouter = Router();
 
 AppRouter.use("/jeddebook", JeddebookRouter);
-AppRouter.use("/user", UserRouter);
+AppRouter.use("/user", authMiddleWare, UserRouter);
+AppRouter.use("/auth", AuthRouter);
 
 module.exports = { AppRouter };
